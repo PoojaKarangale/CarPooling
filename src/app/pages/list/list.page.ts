15b7f8fb16fcb315a1089bd5e08daa370971ce8b
chetnag <chetnaganatra913@gmail.com>
2019-03-02 22:58:40 +0530

    search_post_page css

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams, LoadingController } from '@ionic/angular';
+import * as firebase from 'firebase';
+import { Todo, TodoService } from 'src/app/services/todo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -6,6 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
+  todo: Todo = {
+    task: 'your ride',
+    createAt: new Date().getTime(),
+    priority: null,
+    name: null,
+    lastName: null,
+    gender: null,
+    email: null,
+    mobile: null,
+
+    start: null,
+    end: null,
+    timing: null,
+    vehical: null,
+    seat: null,
+  }
+  todoId = null;
+  /*
   private selectedItem: any;
   private icons = [
     'flask',
@@ -20,18 +42,60 @@ export class ListPage implements OnInit {
     'build'
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
+  */
+
+
+  constructor(private todoService: TodoService,
+    private route: ActivatedRoute,
+    private loadingController: LoadingController,
+    private nav: NavController) {
+    /*
     for (let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Item ' + i,
         note: 'This is item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
-    }
+    }*/
   }
 
   ngOnInit() {
+    this.todoId = this.route.snapshot.params['id'];
+    if (this.todoId) {
+      this.loadTodo();
+    }
   }
+
+  async loadTodo() {
+    const loading = await this.loadingController.create({
+      message: ''
+    });
+    await loading.present();
+    this.todoService.getTodo(this.todoId).subscribe(rec => {
+      loading.dismiss();
+      this.todo = rec;
+    });
+  }
+  async saveTodo() {
+    const loading = await this.loadingController.create({
+      message: ''
+    });
+    await loading.present();
+
+    if (this.todoId) {
+      this.todoService.updateTodo(this.todo, this.todoId).then(() => {
+        loading.dismiss();
+        this.nav.navigateForward('/info-post');
+      });
+    } else {
+      this.todoService.addTodo(this.todo).then(() => {
+        loading.dismiss();
+        this.nav.navigateForward('/info-post');
+      });
+    }
+
+  }
+
   // add back when alpha.4 is out
   // navigate(item) {
   //   this.router.navigate(['/list', JSON.stringify(item)]);
